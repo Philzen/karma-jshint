0f67836c84198baccc926cff0350c29da3bb1d91
Philzen <phil@patient-radio.de>
2014-04-02 20:15:22 +0200

    + jshint options can be configured via Karma Config now

--- README.md
+++ README.md
@@ -1,30 +1,63 @@
-# karma-jshint-preprocessor
+# karma-jshint
 
 > Preprocessor to check JavaScript syntax on the fly.
 
 ## Installation
 
-The easiest way is to keep `karma-jshint-preprocessor` as a devDependency in
-your `package.json`.
-
-```json
-{
-  "devDependencies": {
-    "karma": "~0.10",
-    "karma-jshint-preprocessor": "~0.1"
-  }
-}
-```
-
-You can simply do it by:
-
 ```bash
 npm install karma-jshint-preprocessor --save-dev
 ```
 
+## Usage
+
+### Enabling JSLint'ing of your code files
+
+In your Karma config file (nowadays commonly named `karma.conf.js`),
+specify the files you want jslint'ed in the preprocessor section like this:
+
+``` javascript
+    ...
+    preprocessors: {
+        ...
+        './src/**/*': ['jshint']
+    },
+    ...
+```
+
+### Set [JSHint config options]
+
+You may set your own options by adding a `jshint` section
+to your Karma config file :
+
+``` javascript
+    ...
+    jshint: {
+        options: {
+            curly: true,
+            eqeqeq: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            sub: true,
+            undef: true,
+            boss: true,
+            devel: true,
+            eqnull: true,
+            browser: true,
+            globals: {
+                cordova: true,
+                jQuery: true
+            }
+        }
+    },
+    ...
+```
+
 ----
 
-For more information on Karma see the [homepage].
+For more information on Karma see the [official Karma runner homepage].
 
 
-[homepage]: http://karma-runner.github.com
+[official Karma runner homepage]: http://karma-runner.github.com
+[JSHint config options]: http://www.jshint.com/docs/options/
--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 (function (){
   'use strict';
-  var JSHintReporter = function(loggerFactory) {
+
+    // TODO (feature) get fs and conditionally load .jshintrc file
+
+    // TODO (feature) implement test group definition similar to grunt-contrib-jshint
+
+  var KarmaJshintReporter = function(karmaConfig, loggerFactory) {
     var jshint, log;
     jshint = require('jshint').JSHINT;
     log = loggerFactory.create('preprocessor.jshint');
 
     return function(content,file,done){
-      var i, errors;
+      var i, errors, options = {};
       log.debug('Processing "%s".', file.originalPath);
-      if(!jshint(content)) {
+
+      if (karmaConfig.jshint !== undefined && typeof karmaConfig.jshint.options === 'object') {
+          options = karmaConfig.jshint.options;
+      }
+
+      if(!jshint(content, options)) {
         errors = jshint.data().errors;
         for(i=0;i<errors.length;i++){
           log.error(
-            file.originalPath +
-            ': line ' +
-            errors[i].line +
-            ', col ' +
-            errors[i].character +
-            ', ' +
-            errors[i].reason +
-            ' \n`' + errors[i].evidence + '`'
+            file.originalPath + ': line ' + errors[i].line +
+            ', col ' + errors[i].character +
+            ', ' + errors[i].reason + ' \n`' +
+            errors[i].evidence + '`'
           );
         }
       }
-      
+
       return done(content);
     };
 
   };
 
-  JSHintReporter.$inject = ['logger'];
+  KarmaJshintReporter.$inject = ['config', 'logger'];
 
   module.exports = {
-    'preprocessor:jshint': ['factory', JSHintReporter]
+    'preprocessor:jshint': ['factory', KarmaJshintReporter]
   };
 }).call(this);
